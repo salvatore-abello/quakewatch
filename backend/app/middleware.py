5ed25a292b27a416fadd81bb0591056a7823bb50
Salvatore Abello <107145304+salvatore-abello@users.noreply.github.com>
2024-03-25 00:54:30 +0100

    frontend pt. 1

--- backend/app/middleware.py
+++ backend/app/middleware.py
@@ -10,15 +10,15 @@ def earthquake_query(data):
     # TODO: Check if GET requests are enough
     return requests.request("GET", CONSTANTS.EARTHQUAKE_API_ENDPOINT, params=data).json()
 
-@cached(cache=TTLCache(maxsize=640*512, getsizeof=len, ttl=2 * 60 * 60), lock=Lock()) # TTL 1h
-def weather_query(data):
-    # pioggia moderata (4 – 6 mm/h) pioggia forte (> 6 mm/h) rovescio (> 10 mm/h) nubifragio (> 30 mm/h)
-    # https://api.3bmeteo.com/mobilev3/api_previsioni/home_geo/37.4226711/-122.0849872/en/0/1?format=json2&X-API-KEY=fhrwRdevqwq8r7q9UXTwP6lSX74g34jnQ6756tGo
-    # https://api.3bmeteo.com/mobilev3/api_previsioni/esaorario/5375480/1085/0/6/en/?format=json2&X-API-KEY=fhrwRdevqwq8r7q9UXTwP6lSX74g34jnQ6756tGo
-    # X-Api-Key: fhrwRdevqwq8r7q9UXTwP6lSX74g34jnQ6756tGo
-    # format: json2
-    # 
-    return {}
+# @cached(cache=TTLCache(maxsize=640*512, getsizeof=len, ttl=2 * 60 * 60), lock=Lock()) # TTL 1h
+# def weather_query(data):
+#     # pioggia moderata (4 – 6 mm/h) pioggia forte (> 6 mm/h) rovescio (> 10 mm/h) nubifragio (> 30 mm/h)
+#     # https://api.3bmeteo.com/mobilev3/api_previsioni/home_geo/37.4226711/-122.0849872/en/0/1?format=json2&X-API-KEY=fhrwRdevqwq8r7q9UXTwP6lSX74g34jnQ6756tGo
+#     # https://api.3bmeteo.com/mobilev3/api_previsioni/esaorario/5375480/1085/0/6/en/?format=json2&X-API-KEY=fhrwRdevqwq8r7q9UXTwP6lSX74g34jnQ6756tGo
+#     # X-Api-Key: fhrwRdevqwq8r7q9UXTwP6lSX74g34jnQ6756tGo
+#     # format: json2
+#     # 
+#     return {}
 
 async def dispatch(query_type: str, data):
     if not (func:=ALLOWED_QUERY_TYPES.get(query_type)):
@@ -27,5 +27,5 @@ async def dispatch(query_type: str, data):
     
 ALLOWED_QUERY_TYPES = {
     "earthquake": earthquake_query,
-    "weather": weather_query
+    # "weather": weather_query
 }
