726a5f9a6a0c232e486908af9a9bb15449758c06
Salvatore Abello <107145304+salvatore-abello@users.noreply.github.com>
2024-04-16 16:15:39 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone https://github.com/salvatore-abello/quakewatch/
-docker compose -f ./quakewatch/docker-compose.yml up --build
+docker compose -f ./quakewatch/prod/docker-compose.yml up --build
